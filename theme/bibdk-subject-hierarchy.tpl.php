c8de7cc8162ceceae0d6c27b0c480737c5d006e3
JrgenGNielsen <jgn@dbc.dk>
2013-10-25 14:26:52 +0200

    set styling for top level

--- theme/bibdk-subject-hierarchy.tpl.php
+++ theme/bibdk-subject-hierarchy.tpl.php
@@ -5,29 +5,25 @@
  * Theme implementation for bibdk_subject_hierarchy.
  */
 
-dpm($variables['hierarchy']);
+// dpm($variables['hierarchy'][4]);
 
 ?>
 
-<div id="bibdk-subject-hierarchy">
+<div id="bibdk-subject-hierarchy" class="clearfix">
 
-  <h3>foobar block</h3>
+  <h3><?php echo t('LABEL_SUBJECT_HIERARCHY_HEADER', array(), array('context' => 'bibdk_subject_hierarchy')); ?></h3>
 
 <?php foreach ($variables['hierarchy'] as $key => $item) {
-
-  $attributes['id'][] = 'bibdk-subject-hierarchy-item-0-' . $key;
+  $attributes['id']       = array('bibdk-subject-hierarchy-item-0-' . $key);
+  $attributes['class']    = array('bibdk-subject-hierarchy-item');
   $attributes['data-rel'] = 'subject-hierarchy';
-  $attributes['class'][] = 'bibdk-subject-hierarchy-item';
   $url = 'search/' . $variables['path'] . '/' . trim($item['cql']);
   $query = NULL;
 ?>
 
-  <div class="subject-hierarchy-item hierarchy-level0">
-
-<?php
-    echo l($item['ord'], $url . $query, array('attributes' => $attributes));
-?>
-
+  <div class="span6 subject-hierarchy-item hierarchy-level0">
+    <p class="header"><?php echo l($item['ord'], $url . $query, array('attributes' => $attributes)); ?><p>
+    <p class="note"><?php echo t($item['note'], array(), array('context' => 'bibdk_subject_hierarchy')); ?></p>
   </div>
 
 <?php } ?>
