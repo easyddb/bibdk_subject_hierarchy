e464e7e5fce0ebda427386ca581bffb0fe7fe166
Martin Møller <mmj@dbc.dk>
2014-02-06 10:56:16 +0100

    Template refactor++

--- theme/bibdk-subject-hierarchy-row.tpl.php
+++ theme/bibdk-subject-hierarchy-row.tpl.php
@@ -1,19 +1,16 @@
-<?php #dpm($variables); ?>
 <div class="themes_row clearfix">
-
-  <?php foreach ($items as $item): ?>
-    <?php $index = $item['index']; ?>
-    <div class="themes__item item-<?php print $row; ?>">
+  <?php foreach ($items as $index => $item): ?>
+    <div class="<?php print $item['wrapper_classes']; ?>">
       <p>
         <span class="header">
-          <?php print "<a href='$baseurl/$index' id='subject-hierarchy-label-link-0-$index' class='use-ajax'>" ?>
+          <a href="<?php print $item['url']; ?>" id="<?php print $item['label_id']; ?>" class="use-ajax" >
           <?php print $item['ord']; ?>
           </a>
         </span>
 
         <span class="note">
-          <?php print "<a href='$baseurl/$index' id='subject-hierarchy-note-link-0-$index' class='use-ajax'>" ?>
-          <?php print t($item['note'], array(), array('context' => 'bibdk_subject_hierarchy')); ?>
+          <a href="<?php print $item['url']; ?>" id="<?php print $item['note_id']; ?>" class="use-ajax" >
+          <?php print $item['note']; ?>
           </a>
         </span>
       </p>
--- theme/bibdk-subject-hierarchy.tpl.php
+++ theme/bibdk-subject-hierarchy.tpl.php
@@ -7,14 +7,11 @@
 ?>
 
 <div id="bibdk-subject-hierarchy" class="themes clearfix">
-
   <div class="container clearfix">
-
     <div class="bibdk-subject-hierarchy-header">
       <?php echo t($header, array(), array('context' => 'bibdk_subject_hierarchy')); ?>
     </div>
-
-    <?php foreach($rows as $row) : ?>
+    <?php foreach ($rows as $row) : ?>
       <?php print $row; ?>
     <?php endforeach; ?>
   </div>
