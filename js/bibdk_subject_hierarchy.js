451c29c91dbe311181e35b14d1b54d592636141a
Martin Møller <mmj@dbc.dk>
2014-02-06 09:45:44 +0100

    Cleanup

--- js/bibdk_subject_hierarchy.js
+++ js/bibdk_subject_hierarchy.js
@@ -7,28 +7,6 @@
                 $('.themes__item').removeClass('themes__item--active');
                 $('.themes__sublists__wrapper').fadeOut('200');
             });
-
-          /*
-          $('#columns-wrapper', context).find('.themes__sublists__wrapper.row-1').css({
-            'display': 'block',
-            'background-color': 'red',
-            'height':'100px'
-          });
-
-          $('#subjects-wrapper', context).find('.themes__sublists__wrapper.row-1').css({
-            'display': 'block',
-            'background-color': 'red',
-            'height':'100px'
-          });*/
-
-          //TODO mmj if .themes__sublists__wrapper has content it should be displayed
-          /*$('.themes__sublists__wrapper').each(function(index, element){
-            if(element.innerHTML.length){
-              $(element).css({
-                'display':'block'
-              });
-            }
-          });*/
         }
     };
 }(jQuery));
