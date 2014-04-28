570a0dc83764cd628affbdbe383c3db169345731
Søren Vibjerg <vibjerg@gmail.com>
2014-04-28 11:07:25 +0200

    hierarchy search functionality added

--- js/bibdk_subject_hierarchy.js
+++ js/bibdk_subject_hierarchy.js
@@ -6,6 +6,12 @@
                 $('.themes__item').removeClass('themes__item--active');
                 $('.themes__sublists__wrapper').fadeOut('200');
             });
+            $('#bibdk-subject-hierachy-searchfield-form', context).ajaxComplete(function(event, xhr, settings) {
+                if(event.target.id == 'bibdk-subject-hierachy-searchfield-form'){
+                    console.log(xhr);
+                }
+
+            });
         }
     };
 }(jQuery));
